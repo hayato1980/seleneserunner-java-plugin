9624a952d2c1e590afea0c908f6dd21fd0c444b9
Hayato ITO <haya10.ito@gmail.com>
2012-09-01 16:38:14 +0900

    fix: fail test on no display or no firefox environment.

--- src/test/java/jp/haya10/jenkins/seleneserunnerplugin/SeleneseRunnerBuilderTest.java
+++ src/test/java/jp/haya10/jenkins/seleneserunnerplugin/SeleneseRunnerBuilderTest.java
@@ -8,23 +8,33 @@ import hudson.model.FreeStyleProject;
 
 import java.io.File;
 
-import jp.haya10.jenkins.seleneserunnerplugin.SeleneseRunnerBuilder;
+import jp.vmi.selenium.webdriver.DriverOptions;
 import jp.vmi.selenium.webdriver.WebDriverManager;
 
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.junit.internal.AssumptionViolatedException;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.openqa.selenium.WebDriverException;
 
-public class SeleneseRunnerBuilderTest extends HudsonTestCase {
+public class SeleneseRunnerBuilderTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    private static boolean noDisplay = false;
+
     @Test
     public void testRunSelenese() throws Exception {
-        FreeStyleProject p = createFreeStyleProject();
+        FreeStyleProject p = j.createFreeStyleProject();
         String file = TestUtils.getScriptFile(this.getClass(), "Simple");
         p.getBuildersList().add(
             new SeleneseRunnerBuilder(file, WebDriverManager.FIREFOX, true, ""));
 
         assertThat(new File(file).exists(), is(true));
         try {
-            assertBuildStatus(Result.SUCCESS, p.scheduleBuild2(0).get());
+            j.assertBuildStatus(Result.SUCCESS, p.scheduleBuild2(0).get());
         } finally {
             for (String log : p.getLastBuild().getLog(100)) {
                 System.out.println(log);
@@ -33,5 +43,30 @@ public class SeleneseRunnerBuilderTest extends HudsonTestCase {
 
         FilePath screenshot = p.getSomeWorkspace().child("screenshots");
         assertThat(screenshot.list().isEmpty(), is(false));
+    }
+
+    /**
+     * Check Firefox connected.
+     */
+    @Before
+    public void assumeConnectFirefox() throws Exception {
+        if (noDisplay)
+            throw new AssumptionViolatedException("no display specified");
+
+        setupWebDriverManager();
+        try {
+            WebDriverManager.getInstance().get();
+        } catch (WebDriverException e) {
+            if (e.getMessage().contains("no display specified")) {
+                noDisplay = true;
+                Assume.assumeNoException(e);
+            }
+        }
+    }
+
+    private void setupWebDriverManager() {
+        WebDriverManager manager = WebDriverManager.getInstance();
+        manager.setWebDriverFactory(WebDriverManager.FIREFOX);
+        manager.setDriverOptions(new DriverOptions());
     }
 }
