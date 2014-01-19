cee43ae088f018e23b34e061da0ea2728775c828
Hayato ITO <haya10.ito@gmail.com>
2014-01-19 17:21:49 +0900

    fix compile error

---
+++ src/main/java/jp/haya10/jenkins/seleneserunnerplugin/PathUtils.java
@@ -0,0 +1,29 @@
+package jp.haya10.jenkins.seleneserunnerplugin;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+
+public class PathUtils {
+
+    public static List<File> searchExecutableFile(final String filename) {
+        List<File> result = new LinkedList<File>();
+        for (String path : System.getenv("PATH").split(Pattern.quote(File.pathSeparator))) {
+            File dir = new File(path);
+            if (dir.isFile())
+                dir = dir.getAbsoluteFile().getParentFile();
+            result.addAll(Arrays.asList(dir.listFiles()));
+        }
+
+        return new LinkedList<File>(Collections2.filter(result, new Predicate<File>() {
+            public boolean apply(File file) {
+                return file.canExecute() && file.getName().equals(filename);
+            }
+        }));
+    }
+}
